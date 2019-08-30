cf1424260f2211fbf75e1d7971cf7c9309b16c69
Deepak Rai <Deepakrai767622@gmail.com>
2019-08-30 18:05:46 +0530

    friday work
    html header and hero and css done working for menu javascript ...

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
 //start
+
+// function expression to select element
+const  selectElement = (s) => document.querySelector(s);
+//open the menu on click
+selectElement( s '.open').addEventListener('click', () => {
+    selectElement( s;)
+})
