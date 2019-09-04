097366c05d8a10f4003abcbecb02789913a9a427
Deepak Rai <Deepakrai767622@gmail.com>
2019-09-04 12:00:38 +0530

    today
    end

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@
 // function expression to select element
 const  selectElement = (s) => document.querySelector(s);
 //open the menu on click
-selectElement( s '.open').addEventListener('click', () => {
-    selectElement( s;)
+selectElement('.open').addEventListener('click', () => {
+    selectElement('.nav-list').classList.add('active');
+})
+//close the menu on click
+selectElement('.close').addEventListener('click', () => {
+    selectElement('.nav-list').classList.remove('active');
 })
